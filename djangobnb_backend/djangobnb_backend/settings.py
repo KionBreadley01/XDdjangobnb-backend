a60e6111d172bfe86201eaaed21942d1a926f3ca
KionBreadley01 <juansanchezjuan125@gmail.com>
2025-03-16 18:15:43 -0600

    Get properties in the front end list them

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -50,9 +50,12 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000",
+    "http://localhost:3000",  # Si tienes una aplicación frontend en localhost:3000
+    "http://127.0.0.1:8000",  # Si estás trabajando con la API localmente
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_AUTH ={
     "USE_JWT": True,
